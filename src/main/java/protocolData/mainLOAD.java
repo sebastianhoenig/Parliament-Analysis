8795adb284d2db688cac1460f193aaa8bf98c719
Sebastian Hönig <hoenigsebastian@gmail.com>
2022-02-02 12:54:25 +0100

    quick fix

--- src/main/java/protocolData/mainLOAD.java
+++ src/main/java/protocolData/mainLOAD.java
@@ -176,7 +176,7 @@ public class mainLOAD {
                 String zipString = String.valueOf(out);
                 boolean containsStr = zipString.contains("wp19");
                 if (containsStr) {
-                    UnzipFile("src/main/resources/loadedProtocols/" + name + "." + format,"src/main/resources/loadedProtocols/" + name + ".XML");
+                    UnzipFile.unzip("src/main/resources/loadedProtocols/" + name + "." + format,"src/main/resources/loadedProtocols/" + name + ".XML");
                 }
             } catch (IOException ex) {
                 ex.printStackTrace(); }
@@ -232,55 +232,5 @@ public class mainLOAD {
     }
 
 //TODO : 4.   zip entpacken
-    public void UnzipFile(String source, String target) {
-        this.source = source;
-        this.target = target;
-    }
-
-    public void unzip() {
-        Path mySource = Paths.get(this.source);
-        Path myTarget = Paths.get(this.target);
-        try (ZipInputStream zis = new ZipInputStream(new FileInputStream(mySource.toFile()))) {
-            ZipEntry zipEntry = zis.getNextEntry();
-
-            while (zipEntry != null) {
-                boolean isDirectory = false;
-                if (zipEntry.getName().endsWith(File.separator)) {
-                    isDirectory = true;
-                }
-                Path newPath = zipSlipProject(zipEntry, myTarget);
-
-                if (isDirectory) {
-                    Files.createDirectories(newPath);
-                }
-                else {
-                    if (newPath.getParent() != null) {
-                        if (Files.notExists(newPath.getParent())) {
-                            Files.createDirectories(newPath.getParent());
-                        }
-                    }
-                    Files.copy(zis, newPath, StandardCopyOption.REPLACE_EXISTING);
-                }
-                zipEntry = zis.getNextEntry();
-            }
-            zis.closeEntry();
-        }
-        catch(IOException ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    public Path zipSlipProject(ZipEntry zipEntry, Path targetDir)
-            throws IOException {
-
-        Path targetDirResolved = targetDir.resolve(zipEntry.getName());
-
-        Path normalizePath = targetDirResolved.normalize();
-        if (!normalizePath.startsWith(targetDir)) {
-            throw new IOException("Bad zip entry: " + zipEntry.getName());
-        }
-
-        return normalizePath;
-    }
 
 }
