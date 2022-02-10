0652e4ffdf9313f26071bc01ba16802aad3c7a5d
benwerner <Jlki1234!>
2022-02-10 14:33:50 +0100

    adding /token/speakerID /token/party

--- src/main/java/database/ApiConnection.java
+++ src/main/java/database/ApiConnection.java
@@ -66,5 +66,152 @@ public class ApiConnection {
             stop();
             return "Shutdown Server";
         });
+
+        // TODO: beginDaten and endDate
+        get("/token", (request, response) -> {
+            response.type("application/json;charset=UTF-8");
+
+            String speakerID = request.queryMap().get("speakerID").value();
+            String party = request.queryMap().get("party").value();
+            Document doc;
+            MongoCursor cursor = null;
+            HashMap<String, Integer> allToken = new HashMap<>();
+            if (! request.queryMap().hasKeys()) {
+                Document project = Document.parse("{$project: {\"token\": \"$token\"}}");
+                cursor = handler.getCollection("aspeeches").aggregate(
+                        Arrays.asList(project)).cursor();
+            } else if (speakerID != null && party == null) {
+                doc = handler.getDBDocument(speakerID, "amembers");
+                if (doc == null) {
+                    JSONObject json = new JSONObject();
+                    json.put("message", String.format("No user with id '%s' found", speakerID));
+                    response.status(400);
+                    return json.toJSONString();
+                }
+                Document match = Document.parse("{$match : {\"speaker\" : \"" + speakerID +"\" }}");
+                Document group = Document.parse("{$group: {_id : \"$speaker\", allToken : {$push : \"$token\"}}}");
+                Document project = Document.parse("{$project : {" +
+                        "_id : \"$_id\"," +
+                        "token : {" +
+                        "$reduce : {" +
+                        "input:\"$allToken\"," +
+                        "initialValue: [ ]," +
+                        "in: { $concatArrays: [ \"$$value\", \"$$this\" ] }" +
+                        "}}}}");
+                cursor = handler.getCollection("aspeeches").aggregate(
+                        Arrays.asList(
+                                match,
+                                group,
+                                project
+                        )
+                ).cursor();
+            } else if (speakerID == null && party != null) {
+                doc = (Document) handler.getCollection("amembers").aggregate(
+                        Arrays.asList(
+                                Document.parse("{$match: {\"party\" : \"" + party + "\"}}"),
+                                Document.parse("{$group:{_id: \"$party\", ids: { $push:  \"$_id\" }}}")
+                        )
+                ).first();
+                if (doc != null) {
+                    List<String> idList = (ArrayList<String>) doc.get("ids");
+                    idList = idList.stream().map((String id) -> id = "\"" + id + "\"").collect(Collectors.toList());
+                    cursor = handler.getCollection("aspeeches").aggregate(
+                            Arrays.asList(
+                                    Document.parse("{$match: {\"speaker\" : {$in : " + idList + "}}}"),
+                                    Document.parse("{$project : {_id: \"all\", token : \"$token\"}}")
+                            )
+                    ).cursor();
+                } else {
+                    JSONObject json = new JSONObject();
+                    json.put("message", String.format("No party with name '%s' found", party));
+                    response.status(400);
+                    return json.toJSONString();
+                }
+            } else {
+                JSONObject json = new JSONObject();
+                json.put("message", "URL is wrong!");
+                response.status(400);
+                return json.toJSONString();
+            }
+            while (cursor.hasNext()) {
+                doc = (Document) cursor.next();
+                ArrayList<String> tokenList = (ArrayList<String>) doc.get("token");
+                for (String token : tokenList) {
+                    if (allToken.containsKey(token)) {
+                        allToken.put(token, allToken.get(token) + 1);
+                    } else {
+                        allToken.put(token, 1);
+                    }
+                }
+            }
+            LinkedHashMap<String, Integer> allTokenSorted = sortMap(allToken);
+            ArrayList<JSONObject> resultList = new ArrayList<>();
+            for (Map.Entry<String, Integer> entry : allTokenSorted.entrySet()) {
+                String key = entry.getKey();
+                Integer value = entry.getValue();
+                JSONObject json = new JSONObject();
+                json.put("token", key);
+                json.put("count", value);
+                resultList.add(json);
+            }
+            JSONObject result = new JSONObject();
+            result.put("result", resultList);
+            return result.toJSONString();
+        });
+
+    }
+    /**
+     * sorting a HashMap<String, Integer>
+     * @param hashMap
+     * @return
+     */
+    private static LinkedHashMap<String, Integer> sortMap(HashMap<String, Integer> hashMap) {
+
+        LinkedHashMap<String, Integer> sortedMap = new LinkedHashMap<>();
+        List<Map.Entry<String, Integer>> hashMapList = new LinkedList<>(hashMap.entrySet());
+
+        hashMapList.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+
+        for (Map.Entry<String, Integer> entry : hashMapList) {
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }
+        return sortedMap;
     }
 }
+//db.amembers.aggregate( [
+//        {
+//        $match: {"party" : "CDU"}
+//        },
+//        {
+//        $group:
+//        {
+//        _id: "$party",
+//        ids: { $push:  "$_id" }
+//        }
+//        }
+//        ] )
+
+//db.speeches.aggregate( [
+//        {
+//        $match: {"speaker" : "11001478"}
+//        },
+//        {
+//        $group:
+//        {
+//        _id: "$speaker",
+//        allToken: { $push:  "$token" }
+//        }
+//        },
+//        {
+//        $project: {
+//        _id: "$_id",
+//        "token": {
+//        $reduce: {
+//        input: "$allToken",
+//        initialValue: [ ],
+//        in: { $concatArrays: [ "$$value", "$$this" ] }
+//        }
+//        }
+//        }
+//        }
+//        ] )
